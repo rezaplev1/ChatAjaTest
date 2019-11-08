d59d4dc5797df5dc9d264394019f1f51bdd3db66
Muhammad Nizar <muhammad.nizar@mncgroup.com>
2019-11-08 15:53:47 +0700

    Initial Commit

---
+++ ChatAjaTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChatAjaTest
+//
+//  Created by Muhammad Nizar on 08/11/19.
+//  Copyright © 2019 Muhammad Nizar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
